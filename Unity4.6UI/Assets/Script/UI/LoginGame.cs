c497bb066ec614ad7f625b62a5f5ad1551d1cc32
zhangf <zhangf911@gmail.com>
2014-12-18 16:08:59 +0800

    1、添加初始工程

---
+++ Unity4.6UI/Assets/Script/UI/LoginGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginGame : MonoBehaviour {
+
+    public InputField username;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Login()
+    {
+        Debug.Log("user : " + username.text + " request login game...");
+    }
+}
